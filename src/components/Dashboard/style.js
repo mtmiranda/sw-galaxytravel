550a79ab63f1d7328c7b3a69d84389ffb484032a
mtmiranda <mattheus_miranda@hotmail.com>
2021-08-14 15:17:02 -0300

    Dashboard initial styles

---
+++ src/components/Dashboard/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const DashboardWrapper = styled.main`
+    padding: 20px;
+`
+
+export const Content = styled.div`
+    max-width: 1120px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: auto;
+    padding: 20px;
+
+    span {
+        margin: 20px 0;
+    }
+`
