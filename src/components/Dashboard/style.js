778ea146a256e6ed1437c6681b5a7648388544cc
mtmiranda <mattheus_miranda@hotmail.com>
2021-08-14 19:56:19 -0300

    New cards styles with resized grid and content

--- src/components/Dashboard/style.js
+++ src/components/Dashboard/style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const DashboardWrapper = styled.main`
     padding: 20px;
+    flex: 1;
 `
 
 export const Content = styled.div`
@@ -16,5 +17,37 @@ export const Content = styled.div`
 
     span {
         margin: 20px 0;
+    }
+`
+
+export const Cards = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 1.5rem;
+`
+
+export const Card = styled.div`
+    .card-item {
+        background: var(--black);
+        color: var(--gray-100);
+        height: auto;
+        padding: 20px;
+        border-radius: 5px;
+        height: 100%;
+
+        & p {
+            line-height: 1.5rem;
+        }
+
+        & p:first-child {
+            font-weight: bold;
+            filter: brightness(1.5);
+        }
+
+        & p:last-child {
+            color: var(--yellow-brand);
+            text-align: center;
+            margin-top: 2rem;
+        }
     }
 `
