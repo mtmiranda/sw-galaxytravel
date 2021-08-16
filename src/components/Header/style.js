6555c67cf86d91ec34fd7b9602d64de100dc0d8c
mtmiranda <mattheus_miranda@hotmail.com>
2021-08-16 07:35:41 -0300

    changing header padding and icon img size

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -7,11 +7,17 @@ export const HeaderWrapper = styled.div`
 export const Content = styled.div`
     max-width: 1120px;
     margin: 0 auto;
-    padding: 2rem 1rem 5rem;
+    padding: 2rem 1rem 2rem;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     color: var(--yellow-brand);
     font-family: Starjedi, sans-serif;
+
+    .header-icon {
+        width: 64px;
+        height: 64px;
+        margin-top: 20px;
+    }
 `
